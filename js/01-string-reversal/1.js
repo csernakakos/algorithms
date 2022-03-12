18da6124040f804e8f46d459d2d1b740eb616a88
csernakakos <csernakakos@gmail.com>
2022-03-12 08:35:54 +0100

    Replaced for loop with for... of loop

--- js/01-string-reversal/1.js
+++ js/01-string-reversal/1.js
@@ -1,9 +1,13 @@
-function reverse(str) {
-    let reversed = [];
-    let arr = str.split("");
+function reverse(string) {
+    let reversed = "";
+    // for (let i = arr.length -1; i >= 0; i--) {
+    //     reversed.push(arr[i]);
+    // };
 
-    for (let i = arr.length -1; i >= 0; i--) {
-        reversed.push(arr[i]);
+    // Avoid the traditional for loop whenever you can. Instead, use a for... of loop:
+    
+    for (let character of string) {
+        reversed = character + reversed;
     };
 
     return reversed;
